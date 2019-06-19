bcd37f744b69aa118ddf90d96d6c8005b379903d
Karen Herrera <837@holbertonschool.com>
2019-06-19 20:07:46 +0000

    6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,17 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	int i;
+	double d;
+	char c;
+	long long l;
+	float f;
+	long int li;
+
+	printf("Size of a char is: %d byte(s) \n", sizeof(c));
+	printf("Size of an int: %d byte(s) \n", sizeof(i));
+	printf("Size of an long int: %d byte(s) \n", sizeof(li));
+	printf("Size of a long long int: %d byte(s) \n", sizeof(l));
+	printf("Size of a float is: %d byte(s) \n", sizeof(f));
 	return (0);
 }
