25f812343e6af1733ef13adec73e918c99700d21
Karen Herrera <837@holbertonschool.com>
2019-06-19 20:18:45 +0000

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,20 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: size of various types
  */
 int main(void)
 {
+	char c;
+	int i;
+	long int li;
+	long long l;
+	float f;
+
+	printf("size of a char: %d byte(s) \n" sizeof(c));
+	printf("size of an int: %d byte(s) \n" sizeof(i));
+	printf("size of a long int: %d byte(s) \n" sizeof(li));
+	printf("size of a long long: %d byte(s) \n" sizeof(l));
+	printf("size of a float: %d byte(s) \n" sizeof(f));
+	return (0);
+}
