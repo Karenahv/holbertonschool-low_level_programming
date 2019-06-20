80b81009e6b409078bf05dce29c4f5ce5ace3bb6
Karen Herrera <837@holbertonschool.com>
2019-06-20 21:28:34 +0000

    10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+	int j;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		for (j = 0; j < 10; j++)
+		{	putchar (ch + '0');
+			putchar (j + '0');
+			if (ch != 9 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
