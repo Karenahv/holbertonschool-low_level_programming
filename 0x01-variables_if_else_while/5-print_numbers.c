d0c880a64011c1a509e58d5eeb3d42d6d66c229e
Karen Herrera <837@holbertonschool.com>
2019-06-20 20:04:16 +0000

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar (ch);
+	putchar('\n');
+	return (0);
+}
