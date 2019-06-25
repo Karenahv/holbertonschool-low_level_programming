2d4c5e190386c182a0868a035c898936a15e7a16
Karen Herrera <837@holbertonschool.com>
2019-06-25 14:41:30 +0000

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Entry point
- *@c: chararter to check
+ *@n: chararter to check
  * Return: if char greater than 0, return 1, equal to 0 return 0, else -1
  */
 int print_sign(int n)
