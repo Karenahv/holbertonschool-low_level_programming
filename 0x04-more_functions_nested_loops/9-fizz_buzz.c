2df0842ae9dff01142b79b413505663cf69f54b3
Karen Herrera <837@holbertonschool.com>
2019-06-27 19:43:14 +0000

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,13 +18,13 @@ int main(void)
 		}
 		if (i % 5 == 0)
 		{
-			printf("buzz");
+			printf("Buzz");
 		}
 		if (i % 3 != 0 && i % 5 != 0)
 		{
 			printf("%d", i);
 		}
-		if ( i != 100)
+		if (i != 100)
 			printf(" ");
 	}
 	putchar('\n');
