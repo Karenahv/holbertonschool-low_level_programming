8043c7681a9f16ae87eb1e82e1f05c0acb98e561
Karen Herrera <837@holbertonschool.com>
2019-06-27 18:55:14 +0000

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_square - Entry point
+ *@size: size of square
+ * Return: no return
+ */
+void print_square(int size)
+{
+	int i;
+	char k;
+	int j;
+
+	k = '#';
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(k);
+		}
+		_putchar('\n');
+	}
+		if (size <= 0)
+		{
+			_putchar('\n');
+		}
+}
