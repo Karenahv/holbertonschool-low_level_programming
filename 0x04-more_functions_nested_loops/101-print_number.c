9b5825d9db038142a15efdd4765e39172047b844
Karen Herrera <837@holbertonschool.com>
2019-06-28 02:07:52 +0000

    12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,39 +7,33 @@
  */
 void print_number(int n)
 {
-	int f;
-	int l;
-	int ff;
-	int fl;
-	int lf;
-	int ll;
 
-	f = n % (100);
-	l = (n - f) / (100);
-	ff = f / 10;
-	fl = f % 10;
-	lf = l / 10;
-	ll = l % 10;
-	if (n > 0 && n < 100)
-	{
-		_putchar(ff + '0');
-		_putchar(fl + '0');
-	}
-	if (n >= 100 && n < 1000)
-	{
-		_putchar(ll + '0');
-		_putchar(lf + '0');
-		_putchar(fl + '0');
-	}
-	if (n >= 1000 && n < 10000)
-	{
-		_putchar(lf + '0');
-		_putchar(fl + '0');
-		_putchar(ff + '0');
-		_putchar(fl + '0');
-	}
-	if (n == 0)
-		_putchar(ff + '0');
+	char l;
+	int r;
+	char d;
 
+	if (n < 0)
+	{
+		_putchar('-');
+		l = ('0'- (n % 10));
+		n /= -10;
+	} else
+	{
+		l = ((n % 10) + '0');
+		n /= 10;
+	}
+	r = 0;
+	while (n > 0)
+	{
+		r = r * 10 + (n % 10);
+		n /= 10;
+	}
 
+	while (r > 0)
+	{
+		d = ((r % 10) + '0');
+		_putchar (d);
+		r /=10;
+	}
+	_putchar(l);
 }
