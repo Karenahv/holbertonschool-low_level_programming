67154babeb62f312bbc0ace5ff0b9b6e67245f63
Karen Herrera <837@holbertonschool.com>
2019-07-02 16:25:37 +0000

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *_strcpy - Entry point
+ *@dest: pointer the first string
+ *@src: pointer the second string
+ *Return: no return
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (*(src + i) != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (j < i)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	return (&*dest);
+}
