6cc41355009ad883fba1834d2e542bd60a84d3a8
Karen Herrera <837@holbertonschool.com>
2019-07-04 19:03:11 +0000

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *_strcmp - compare two strings
+ *@s1: pointer for the first string we want to compare
+ *@s2: pointer for the second string we want to compare
+ *
+ *Return: and integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int j;
+	int entero;
+	entero = 0;
+	for (j = 0; s1[j] != '\0'; j++)
+	{
+		if (s1[j] - s2[j] != 0)
+		{
+			entero = s1[j] - s2[j];
+			break;
+		}
+	}
+	if (entero == 0 && s1[j] == '\0')
+		entero = s1[j] - s2[j];
+	return (entero);
+}
