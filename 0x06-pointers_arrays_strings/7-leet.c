0466f9b08da3e8b792b6dbb927630caa2fd47107
Karen Herrera <837@holbertonschool.com>
2019-07-04 22:12:22 +0000

    number 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@ char *leet(char *p)
 {
 	int j;
 	int i;
-	char a1[] = {'a', 'A', 'E', 'e', 'o', '0', 't', 'T', 'L', 'l'};
+	char a1[] = {'a', 'A', 'E', 'e', 'o', 'O', 't', 'T', 'L', 'l'};
 	char a2[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	for (j = 0; p[j] != '\0'; j++)
@@ -18,7 +18,7 @@ char *leet(char *p)
 		{
 			if (p[j] == a1[i])
 				p[j] = a2[i];
-			}
+		}
 	}
 	return (p);
 }
