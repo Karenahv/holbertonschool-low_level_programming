fc3d9fa4fcbcbdbf667599220b24cbbb7f365949
Karen Herrera <837@holbertonschool.com>
2019-07-04 21:09:59 +0000

    ejercicio 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *cap_string - String to Uppercase
+ *@p: pointer the string we want to capitalize
+ *Return: string
+ */
+char *cap_string(char *p)
+{
+	int j;
+
+	j = 0;
+	while (p[j] != '\0')
+	{
+		if (p[j] == ' ' || p[j] == ';' || p[j] == '\n' || p[j] == '\t'
+		    || p[j] == ',' || p[j] == '.' || p[j] == '!' || p[j] == '?'
+		    || p[j] == '"' || p[j] == '(' || p[j] == ')' ||
+		    p[j] == '{' || p[j] == '}')
+			if (p[j + 1] >= 'a' && p[j + 1] <= 'z')
+			p[j + 1] = p[j + 1] - 32;
+		j++;
+	}
+
+	return (p);
+}
