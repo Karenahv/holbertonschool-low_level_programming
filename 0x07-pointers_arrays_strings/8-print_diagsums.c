f18885b5058ad0af87931da6757b67d3e72b53bc
Karen Herrera <837@holbertonschool.com>
2019-07-08 23:00:53 +0000

    corrección 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,24 +11,20 @@ void print_diagsums(int *a, int size)
 {
 	int i;
 	int sum;
-	int valor;
 	int sum2;
 
 	sum = 0;
-	valor = 0;
 	sum2 = 0;
 	for (i = 0; i < size * size;)
 	{
-		valor = a[i];
-		sum = sum + valor;
+		sum = sum + a[i];
 		i = i + size + 1;
 	}
 
 	for (i = size - 1; i < (size * size) - 1;)
 	{
-		valor = a[i];
-		sum2 = sum2 + valor;
+		sum2 = sum2 + a[i];
 		i = i + (size - 1);
 	}
-	printf("%d,  %d\n", sum, sum2);
+	printf("%d, %d\n", sum, sum2);
 }
