a07ad02ee36e505676089623070d18d8ba8eecae
Karen Herrera <837@holbertonschool.com>
2019-07-12 20:38:18 +0000

    advanced

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *@argc: number of arguments
+ *@argv: string with arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int monedas;
+	int j;
+	int count;
+	int coins[5] = {25, 10, 5, 2, 1};
+
+
+	(void)argv;
+	count = 0;
+	monedas = 0;
+	if (argc == 2)
+	{
+		if (atoi(argv[1]) == 0)
+			return (1);
+		for (j = 0; j <= 4; j++)
+		{
+			if (atoi(argv[1]) >= coins[j])
+			{
+				monedas = atoi(argv[1]) / coins[j];
+				count = atoi(argv[1]) % coins[j];
+				break;
+			}
+		}
+
+		for (j = 0; j <= 4; j++)
+		{
+			if (count >= coins[j])
+				monedas++;
+		}
+	}
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", monedas);
+	return (0);
+}
