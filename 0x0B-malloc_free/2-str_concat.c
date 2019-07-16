9c132d8e4dfef8e9350a1b4401635d2041ffeaf3
Karen Herrera <837@holbertonschool.com>
2019-07-16 18:17:46 +0000

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 		;
 	for (n2 = 0; s2[n2] != '\0'; n2++)
 		;
-	strnew = (char *)malloc((n1 + n2) * sizeof(char));
+	strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
 	if (strnew == NULL)
 	{
 		return (NULL);
@@ -37,6 +37,6 @@ char *str_concat(char *s1, char *s2)
 		strnew[i] = s2[count];
 		count++;
 	}
-	strnew[i] = '\0';
+	strnew[i + 1] = '\0';
 	return (strnew);
 }
