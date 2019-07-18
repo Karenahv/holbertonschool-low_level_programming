6b69bb154e4fb7a863b2e67f0b86c70fc1f976bb
Karen Herrera <837@holbertonschool.com>
2019-07-18 21:49:49 +0000

    100 correction new < old

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -32,7 +32,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		if (new_size < old_size)
 		{
-			t = old_size - new_size;
+			t = new_size;
 			px = (char *)ptr;
 			for (i = 0; i < t; i++)
 				p[i] = px[i];
