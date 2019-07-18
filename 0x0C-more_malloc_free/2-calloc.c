aff02766046b0920bdfc161a2a87129ad1e70890
Karen Herrera <837@holbertonschool.com>
2019-07-18 18:33:48 +0000

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,17 +12,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *array = NULL;
 	unsigned int i;
-	int t;
+	unsigned int t;
 	char *f;
 
-	t = nmemb * size;
-	array= malloc(t);
 	if (size == 0 || nmemb == 0)
 		return (NULL);
+	t = nmemb * size;
+	array = malloc(t);
 	f = (char *)array;
 	if (f != NULL)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i < t; i++)
 			f[i] = 0;
 		return (f);
 	} else
