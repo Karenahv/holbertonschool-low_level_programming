f3d3dccd4270992482ab57f42d29b68fc3c93e82
Karen Herrera <837@holbertonschool.com>
2019-07-22 21:24:00 +0000

    exercise 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,14 +10,14 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if ((*d).name != NULL)
+		if ((*d).name == NULL)
+			printf("(nil)\n");
+		else
 			printf("Name: %s\n", (*d).name);
-		else
-			printf("(nil)\n");
 		printf("Age: %f\n", (*d).age);
-		if ((*d).owner != NULL)
-			printf("Owner: %s\n", (*d).owner);
-		else
+		if ((*d).owner == NULL)
 			printf("(nil)\n");
+		else
+			printf("Owner: %s\n", (*d).owner);
 	}
 }
