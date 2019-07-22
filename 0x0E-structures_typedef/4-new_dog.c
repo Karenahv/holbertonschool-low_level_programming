1f31e2529cdc4459268805a8e57953d2645ea254
Karen Herrera <837@holbertonschool.com>
2019-07-22 23:09:06 +0000

    exercises 4 and 5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - create new dog
+ *@name:char
+ *@age:float
+ *@owner: char
+ * Return: new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	char *name2;
+	char *owner2;
+	int n, o, i, j;
+	struct dog *luna;
+
+	luna = malloc(sizeof(struct dog));
+	if (luna == NULL)
+		return (NULL);
+	for (n = 0; *(name + n) != '\0'; n++)
+		;
+	for (o = 0; *(owner + o) != '\0'; o++)
+		;
+	name2 = malloc(n + 1);
+	if (name2 != NULL)
+	{
+		for (i = 0; i < n; i++)
+			*(name2 + i) = *(name + i);
+	} else
+	{
+		free(luna);
+		return (NULL);
+	}
+	owner2 = malloc(o + 1);
+	if (owner2 != NULL)
+	{
+		for (j = 0; j < o; j++)
+			*(owner2 + j) = *(owner + j);
+	} else
+	{
+		free(luna);
+		free(name2);
+		return (NULL);
+	}
+	luna->name = name2;
+	luna->age = age;
+	luna->owner = owner2;
+	return (luna);
+
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog structure
+ *@d:dog struct
+ * Return: no return
+ */
+void free_dog(dog_t *d)
+{
+
+	if (d != NULL)
+	{
+		free(d);
+		free(d->name);
+		free(d->owner);
+	}
+}
