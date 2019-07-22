b6191d428fed8c1e4a7ccd00090346e155cd460f
Karen Herrera <837@holbertonschool.com>
2019-07-22 21:34:57 +0000

    exercise 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,8 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+}
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-#endif
+typedef struct dog dog_t;
+#endif /* DOG_H */
