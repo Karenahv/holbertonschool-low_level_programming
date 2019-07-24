a5c631a1bf2d74648f4f7e8dabc9252f74e93f8a
Karen Herrera <837@holbertonschool.com>
2019-07-24 18:43:42 +0000

    exercise 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (action != NULL)
+	if (action != NULL && array != NULL)
 	{
 		for (i = 0; i < size; i++)
 			action(array[i]);
