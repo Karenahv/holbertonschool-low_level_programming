cda3b0081f84e9c2ca13ddbffa32d34054f60333
Karen Herrera <837@holbertonschool.com>
2019-07-24 22:52:23 +0000

    advanced

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - searchs for an integer
+ *@argc: number of arguments
+ *@argv: array with arguments
+ *
+ * Return: no return
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (argv[1] < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	return (0);
+}
