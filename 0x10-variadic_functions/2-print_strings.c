a7d457eaba496fe4bf1887bd6ee650a627f92671
Karen Herrera <837@holbertonschool.com>
2019-07-27 19:17:43 +0000

    correction nil exercise 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,13 +13,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int i;
 	char *p;
+	char array[] = "(nil)";
 
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
 		p = va_arg(valist, char *);
 		if (p == NULL)
-			printf("(nil)");
+			p = array;
 		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s%s", p, separator);
