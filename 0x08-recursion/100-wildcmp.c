828080bd28eec082b9199da697c6b4abe6606a00
Karen Herrera <837@holbertonschool.com>
2019-08-01 00:10:37 +0000

    Task 8: Compare strings

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *wildcmp - function that returns the power of number
+ *@s1: string
+ *@s2: string to compare
+ *Return: 1 if the strings are identical or 0 if it is not
+ */
+int wildcmp(char *s1, char *s2)
+{
+
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *(s2 + 1) != '\0' && *s2 == '*')
+		return (0);
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	return (0);
+}
