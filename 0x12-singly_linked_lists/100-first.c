808e5425657ff8aa31bed4bdf1290b47ca4b8230
Karen Herrera <837@holbertonschool.com>
2019-08-01 20:35:47 +0000

    task #5: output

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,7 @@
  */
 void __attribute__((constructor)) calledFirst()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon");
-	printf("my back!\n");
+
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
