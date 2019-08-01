d2d0e6478bc1d1f39339ca6a1a4fd0a9f637edfe
Karen Herrera <837@holbertonschool.com>
2019-08-01 20:22:23 +0000

    task #5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * calledFirst - print elements beforme main
+ *
+ * Return: no return
+ */
+void __attribute__((constructor)) calledFirst()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon");
+	printf("my back!\n");
+}
