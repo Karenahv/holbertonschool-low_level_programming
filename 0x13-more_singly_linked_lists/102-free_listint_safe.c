aae7ae17312e2c26370032be2cebe19284e561cd
Karen Herrera <837@holbertonschool.com>
2019-08-06 21:17:15 +0000

    task #13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - free elements of a list_t list
+ * @h: first element of the list
+ * Return: the size of the list that was free´d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+
+	listint_t *temp;
+	listint_t *aux;
+	size_t i;
+
+	i = 0;
+	temp = *h;
+	while (temp != NULL)
+	{
+		i++;
+		aux = temp;
+		temp = temp->next;
+		free(aux);
+		if (aux < temp)
+		{
+			break;
+		}
+		*h = NULL;
+	}
+	return (i);
+}
