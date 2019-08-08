3a289cc5b803328d7afc8026408a3317d8da56f7
Karen Herrera <837@holbertonschool.com>
2019-08-08 21:28:24 +0000

    task #4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: decimal number
+ * @index: index
+ * Return: 1 if it worked or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int binary, var;
+
+	binary = 0;
+	var = 0;
+	if (index > 63)
+		return (-1);
+	binary = (*n >> index);
+	var = binary | 1;
+	if (var)
+	{
+		*n = ~(~var | 1) << index;
+		return (1);
+	}
+	return (-1);
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -6,4 +6,5 @@ unsigned int _pow(unsigned int num, unsigned int n);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 #endif
