588264ea2c3073cf60dc09082754326666bbd5be
Karen Herrera <837@holbertonschool.com>
2019-08-08 22:47:52 +0000

    task # 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * flip_bits - returns the number of bits I need to flip to get from one number
+ * @n: decimal number 1
+ * @m: decimal number 2
+ * Return: the number of bits I need to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int count, i;
+
+	count = 0;
+	for (i = 0; i < 32; i++)
+	{
+
+		if (((n >> i) & 1) != ((m >> i) & 1))
+		{
+			count++;
+
+		}
+
+	}
+	return (count);
+}
