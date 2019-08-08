1d86b77bf43a6261f57c70855fc7162c6439760b
Karen Herrera <837@holbertonschool.com>
2019-08-08 22:50:04 +0000

    task # 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int count, i;
 
 	count = 0;
-	for (i = 0; i < 32; i++)
+	for (i = 0; i < 64; i++)
 	{
 
 		if (((n >> i) & 1) != ((m >> i) & 1))
