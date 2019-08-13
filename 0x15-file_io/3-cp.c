b14df78bac5c7b54e4c476e2bf9d824c06524f93
Karen Herrera <837@holbertonschool.com>
2019-08-13 22:39:53 +0000

    task # 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,37 +21,37 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97); }
 	fd = open(argv[1], O_RDWR);
 	if (fd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
-	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, 0664);
+	fd2 = open(argv[2], O_CREAT | O_APPEND | O_TRUNC | O_RDWR, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99); }
 	while (sz == 1024)
 	{
 		sz = read(fd, buf, 1024);
 		if (sz == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98); }
 		w = write(fd2, buf, sz);
 		if (w == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99); }}
 	if (close(fd) < 0)
 	{
-		dprintf(2, "Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd);
 		exit(100); }
 	if (close(fd2) < 0)
 	{
-		dprintf(2, "Can't close fd %d\n", fd2);
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd2);
 		exit(100); }
 	return (0);
 }
