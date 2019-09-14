f328a44987a1df0800dd963321dcf10579027200
Karen Herrera <837@holbertonschool.com>
2019-09-14 21:14:31 +0000

    correction #8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -30,35 +30,24 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = temp2;
 		temp2->prev = NULL;
 		free(temp);
-		temp = NULL;
-		return (1);
-	}
+		return (1); }
 	temp4 = *head;
 	for (i = 0; temp4->next != NULL; i++)
 		temp4 = temp4->next;
 	if (index > i)
 		return (-1);
-	while (j < index - 1)
-	{
+	for (j = 0; j < index - 1; j++)
 		temp = temp->next;
-		j++;
-	}
 	if (index == i)
 	{
 		temp2 = temp->next;
 		temp->next = NULL;
 		free(temp2);
-		temp2 = NULL;
-		return (1);
-	}
-	else
-	{
+		return (1); }
 	temp2 = temp->next;
 	temp3 = temp->next->next;
 	temp->next = temp3;
 	temp3->prev = temp;
 	free(temp2);
-	temp2 = NULL;
 	return (1);
-	}
 }
