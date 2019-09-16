ec789532034a0c596e6c681fb2dc229c19e0a050
Karen Herrera <837@holbertonschool.com>
2019-09-16 23:13:51 +0000

    script #4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp https://raw.githubusercontent.com/Karenahv/holbertonschool-low_level_programming/tree/master/0x18-dynamic_libraries/injectcode.so
+wget -P /tmp https://github.com/Karenahv/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/injectcode.so
 export LD_PRELOAD=/tmp/injectcode.so
