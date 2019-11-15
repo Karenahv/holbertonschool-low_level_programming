a02d807fc1737743976567722dd1319523467224
Karen Herrera <837@holbertonschool.com>
2019-11-15 22:45:35 +0000

    5 simple

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,20 +4,10 @@
 
 def island_perimeter(grid):
     p = 0
+    count = 0
     for row in range(len(grid)):
         for col in range(len(grid[row])):
-            if (col == 0 or col == len(grid) or row == 0)\
-               and grid[row][col] == 1:
-                p = 3
-            elif grid[row][col] == 1:
-                p = p + 2
-            elif row != len(grid) and grid[row][col] == 1\
-                 and grid[row + 1][col] == 1:
-                p = p - 1
-            elif col != len(grid[row])\
-                 and row != len(grid) and grid[row][col] == 1\
-                 and grid[row + 1][col] == 1 and grid[row][col + 1] == 1:
-                p = p - 1
-            else:
-                pass
+            if grid[row][col] == 1:
+                count = count + 1
+    p = count * 2
     return (p + 2)
