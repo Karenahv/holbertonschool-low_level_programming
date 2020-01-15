99dfd776c756e52ef7ebe79c0b7db63a0141a5cb
Karen Herrera <837@holbertonschool.com>
2020-01-15 23:59:42 +0000

    se actualiza .h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,7 @@ int jump_search(int *array, size_t size, int value);
 int linear_search2(int *array, size_t size, int value, int imin);
 void  print_array(int *array, size_t l, size_t r);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int binary_search2(int *array, size_t l, size_t size, int value);
 
 #endif
