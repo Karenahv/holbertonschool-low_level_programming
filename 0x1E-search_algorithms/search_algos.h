e345cdd52e96580166cd139cce02c6edff2c0a6e
Karen Herrera <837@holbertonschool.com>
2020-01-15 22:10:00 +0000

    .h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,6 @@ int search_value_half(int *array2, int value, int m);
 int jump_search(int *array, size_t size, int value);
 int linear_search2(int *array, size_t size, int value, int imin);
 void  print_array(int *array, size_t l, size_t r);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
