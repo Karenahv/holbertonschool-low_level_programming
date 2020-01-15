7a16efc071332fce911cdfc0b98adfbcce2d8eaf
Karen Herrera <837@holbertonschool.com>
2020-01-15 17:38:31 +0000

    print format

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,7 +13,7 @@ void print_array(int *array, size_t l, size_t r)
 {
 	printf("Searching in array: ");
 	for (; l < r; l++)
-		printf("%d ", array[l]);
+		printf("%d, ", array[l]);
 	printf("%d\n", array[l]);
 }
 
@@ -32,7 +32,6 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 	r = size - 1;
-
 	for (l = 0; l < size;)
 	{
 		print_array(array, l, r);
